d5cbdf7f5eafba69d8de4bc60ea9a6c07cf91c59
Lunzi QwQ <Lunzi QwQ>
2017-08-03 13:39:29 +0800

    重构优化了部分代码，添加了注释

--- js/config.js
+++ js/config.js
@@ -2,6 +2,25 @@
  * Created by Lunzi on 6/6/2017.
  */
 "use strict";
+
 var size = 50;
+var MIN_SIZE = 50;
+
+var cellSize = 10;
+var MAX_CELLSIZE = 20;
+var MIN_CELLSIZE = 2;
+
+var worldSize = size*cellSize;
+var MAX_WORLDSIZE = 1000;
+var MIN_WORLDSIZE = 500;
+
+
 var density = {value: [300, 500, 1000], mode: 0};    //初始生物密度
+// var density = 300;
+var MAX_DENSITY = 1000;
+var MIN_DENSITY = 300;
+
 var delay = {value: [500, 200, 50], mode:1};      //刷新间隔，单位ms
+// var delay = 200;
+var MAX_DELAY = 500;
+var MIN_DELAY = 50;
--- js/world.js
+++ js/world.js
@@ -5,65 +5,87 @@
 var rule = new Rule();
 var world = new World();
 var template = new Template();
+
+template.updateTemplateList();
+world.init();
+
+//禁用浏览器右键菜单
 $("#world")[0].oncontextmenu = function () {
     return false;
 };
 
-window.addEventListener('mousedown', function (event) {
-    var mousePos = getMousePos(event);
-    var canvasElement = document.getElementById("world");
-    var elementPos = {
-        x1: canvasElement.getBoundingClientRect().left + 10,
-        y1: canvasElement.getBoundingClientRect().top + 10,
-        x2: canvasElement.getBoundingClientRect().left + canvasElement.width + 10,
-        y2: canvasElement.getBoundingClientRect().top + canvasElement.height + 10
-    };
-    if(mousePos.x >= elementPos.x1 && mousePos.x <= elementPos.x2 &&
-        mousePos.y >= elementPos.y1 && mousePos.y <= elementPos.y2){
-        var xx = {
-            x: Math.floor((mousePos.x - elementPos.x1) / 10),
-            y: Math.floor((mousePos.y - elementPos.y1) / 10)
-        };
-        world.setCoordStatus(xx,event.buttons == 1);
-        $("#world").bind('mousemove', function () {
-            var pos = getMousePos();
-            if(pos.x >= elementPos.x1 && pos.x <= elementPos.x2 &&
-                pos.y >= elementPos.y1 && pos.y <= elementPos.y2){
-                var worldCoord = {
-                    x: Math.floor((pos.x - elementPos.x1) / 10),
-                    y: Math.floor((pos.y - elementPos.y1) / 10)
-                };
-                world.setCoordStatus(worldCoord,event.buttons == 1);
-            }
-        });
-    }
+//当按下鼠标按钮时开始绘图
+window.addEventListener('mousedown', function () {
+    drawMousePos();                                     //绘制点击时的点
+    $("#world").bind('mousemove', drawMousePos);        //按下鼠标移动时绑定绘制事件
+    $("#world").bind('mouseenter',function () {         //鼠标按下后再进入canvas区域时绑定绘制事件
+        $("#world").bind('mousemove', drawMousePos);
+    })
 });
+
+//绘制当前鼠标所在的点的Cell
+function drawMousePos() {
+    var elementPos = world.elementPos;
+    var mousePos = getMousePos(event);
+    if(mousePos.x >= elementPos.x1 && mousePos.x <= elementPos.x2 &&
+        mousePos.y >= elementPos.y1 && mousePos.y <= elementPos.y2) {
+        var worldPos = {
+            x: Math.floor((mousePos.x - elementPos.x1) / cellSize),
+            y: Math.floor((mousePos.y - elementPos.y1) / cellSize)
+        };
+        world.setCoordStatus(worldPos, event.buttons == 1);     //绘制当前鼠标所在像素的Cell，左键黑色，其他白色
+    }
+}
+
+//当松开鼠标时，解绑绘制事件
 window.addEventListener('mouseup', function () {
     $("#world").unbind("mousemove");
+    $("#world").unbind("mouseenter");
 });
+
+//当鼠标离开canvas区域时解绑绘制事件
 $("#world").bind('mouseleave', function () {
     $("#world").unbind("mousemove");
 });
 
+//获得当前鼠标相对于页面的位置
 function getMousePos(event) {
     var e = event || window.event;
-    var x = e.clientX;
-    var y = e.clientY;
-    return { 'x': x, 'y': y };
+    return { 'x': e.clientX, 'y': e.clientY };
 }
 
 
-template.updateTemplateList();
-world.init();
 function World() {
-    var space = undefined;
-    var canvas =$("#world")[0].getContext("2d");
-    var running = false;
-    var template = "Random";
+    var space = undefined;                          //Cell数组
+    var canvas =$("#world")[0].getContext("2d");    //Canvas控件
+    var canvasElement = $("#world")[0];             //Canvas元素
+    var isRunning = false;                          //初始游戏状态
+    var template = "Random";                        //初始模板
 
-    this.getSpace = function() {return space;};
+    this.elementPos = getCanvasElementPos();        //Canvas元素位置坐标
+    this.getSpace = function () {return space;}     //space getter
     this.setCoordStatus = function (coord, isLive) {setStatus(coord, isLive);};
+
+    /**
+     * 获取当前Canvas元素位置
+     * @returns {{x1: number coord x1, y1: number coord x2, x2: number coord y1, y2: number coord y2}}
+     */
+    function getCanvasElementPos() {
+        return {
+            x1: canvasElement.getBoundingClientRect().left + 10,
+            y1: canvasElement.getBoundingClientRect().top + 10,
+            x2: canvasElement.getBoundingClientRect().left + canvasElement.width + 10,
+            y2: canvasElement.getBoundingClientRect().top + canvasElement.height + 10
+        };
+    }
+
+    /**
+     * 初始化Canvas元素，控件及模板菜单，更新Canvas元素位置
+     */
     this.init = function () {
+        canvasElement.height = worldSize;
+        canvasElement.width = worldSize;
+
         clearWorld();
         space = new Array(size);
         for(var i = 0; i < size; i++){
@@ -77,41 +99,66 @@ function World() {
                 setStatus(getRandomCoord(),true);
         else
             initTemplate(template);
+
+        this.elementPos = getCanvasElementPos();
     };
 
-    var initTemplate = function (templateName) {
+    /**
+     * 在Canvas中绘制指定的模板
+     * @param templateName 模板名称
+     */
+    function initTemplate(templateName) {
         var temp = new Template().templateList[templateName];
         var off = temp.offsetCoord;
         temp.liveList.forEach(function (x) {
             setStatus({x:x[0]+off.x,y:x[1]+off.y}, true);
         });
-    };
+    }
 
+    /**
+     * 清空所有绘图
+     */
     var clearWorld = function () {
-        canvas.clearRect(0,0,500,500);
+        canvas.clearRect(0,0,worldSize,worldSize);
     };
 
-
+    /**
+     * 将指定坐标的Cell绘制为指定状态
+     * @param coord 坐标
+     * @param isLive Cell状态
+     */
     var setStatus = function(coord, isLive){
         space[coord.x][coord.y].isLive = isLive;
         if (isLive)
-            canvas.fillRect(coord.x*10,coord.y*10,10,10);
+            canvas.fillRect(coord.x*cellSize,coord.y*cellSize,cellSize,cellSize);
         else
-            canvas.clearRect(coord.x*10,coord.y*10,10,10);
+            canvas.clearRect(coord.x*cellSize,coord.y*cellSize,cellSize,cellSize);
     };
 
+    /**
+     * 获得一个world数组范围内状态为死亡的Cell随机坐标
+     * @returns {{x: number coord x, y: number coord y}}
+     */
     var getRandomCoord = function () {
         var coord ={ x:0, y:0};
-        coord.x = Math.floor(Math.random()*50);
-        coord.y = Math.floor(Math.random()*50);
+        coord.x = Math.floor(Math.random()*size);
+        coord.y = Math.floor(Math.random()*size);
         if(isLegal(coord))  return coord;
         else return getRandomCoord();
     };
 
+    /**
+     * 判断指定坐标的Cell是否为存活状态
+     * @param coord 坐标
+     * @returns {boolean} 是否存活
+     */
     var isLegal = function(coord){
         return !space[coord.x][coord.y].isLive;
     };
 
+    /**
+     * 绘制一次world，若游戏状态为开始，按照指定delay不断刷新world
+     */
     var updateSpace = function () {
         var temp = {};
         $.extend(true,temp,space);
@@ -125,32 +172,49 @@ function World() {
                 setStatus({x:i,y:j},temp[i][j].isLive);
             }
         }
-        if(running)
+        if(isRunning)
             setTimeout(updateSpace,delay.value[delay.mode]);
     };
 
+    /**
+     * 将游戏状态设为开始，刷新World
+     */
     this.onStartClick = function () {
-        if(running) return;
-        running = true;
+        if(isRunning) return;
+        isRunning = true;
         updateSpace();
     };
 
+    /**
+     * 将游戏状态设为暂停
+     */
     this.onPauseClick = function () {
-        running = false;
+        isRunning = false;
     };
 
+    /**
+     * 更改刷新delay
+     * @param value delay编号
+     */
     this.speedOnChange = function (value) {
         delay.mode = value;
     };
 
+    /**
+     * 更改模板并重置World
+     * @param value 模板编号
+     */
     this.templateOnChange = function (value) {
         template = value;
         this.onResetClick();
     };
 
+    /**
+     * 重置World并将游戏状态设为暂停
+     */
     this.onResetClick = function () {
         density.mode = $("#density").val();
-        running = false;
+        isRunning = false;
         setTimeout(world.init,delay.value[delay.mode]+10);//等待settimeout的更新方法停止再重置世界。
     };
 }
